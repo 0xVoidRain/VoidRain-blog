bb64950f85e1bf792196c945a885559eb8b1da60
0xVoidRain <betterme365d@gmail.com>
2025-03-17 16:23:54 +0800

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,8 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import DataFlowBackground from '@/components/DataFlowBackground'
+import LayoutWrapper from '@/components/LayoutWrapper'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -96,12 +98,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
+          <DataFlowBackground />
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
-            <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
-              <Header />
-              <main className="mb-auto">{children}</main>
-            </SearchProvider>
+            <LayoutWrapper>
+              <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+                <Header />
+                <main className="mb-auto">{children}</main>
+              </SearchProvider>
+            </LayoutWrapper>
             <Footer />
           </SectionContainer>
         </ThemeProviders>
