64016ce78e9470f910939fc79360b8578ff041d8
0xVoidRain <betterme365d@gmail.com>
2025-05-01 14:31:39 +0800

    Add files via upload

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -1,43 +1,34 @@
-'use client'
-
-import { useEffect, useState } from 'react'
+import { AlgoliaButton } from 'pliny/search/AlgoliaButton'
+import { KBarButton } from 'pliny/search/KBarButton'
+import siteMetadata from '@/data/siteMetadata'
 
 const SearchButton = () => {
-  const [mounted, setMounted] = useState(false)
+  if (
+    siteMetadata.search &&
+    (siteMetadata.search.provider === 'algolia' || siteMetadata.search.provider === 'kbar')
+  ) {
+    const SearchButtonWrapper =
+      siteMetadata.search.provider === 'algolia' ? AlgoliaButton : KBarButton
 
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) return null
-
-  const handleClick = () => {
-    if (typeof window !== 'undefined' && window.toggleSearch) {
-      window.toggleSearch()
-    }
+    return (
+      <SearchButtonWrapper aria-label="Search">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-5 w-5 text-gray-900 dark:text-gray-100 sm:h-6 sm:w-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </SearchButtonWrapper>
+    )
   }
-
-  return (
-    <button
-      type="button"
-      className="mr-1 ml-1 h-8 w-8 rounded p-1 sm:ml-4"
-      aria-label="搜索"
-      onClick={handleClick}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="text-gray-900 dark:text-gray-100"
-      >
-        <path
-          fillRule="evenodd"
-          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </button>
-  )
 }
 
 export default SearchButton
